c04613d787828bed37a7ea55bb03ec1f539e73e4
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-11-22 18:47:32 +0100

    Added SettingsIcon to Home View

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,4 +1,6 @@
+import SettingsIcon from "@/assets/icons/settings-icon.svg";
 import Button from "@/components/Button";
+
 import SearchInput from "@/components/SearchInput";
 import { useRouter } from "expo-router";
 import { StyleSheet, View } from "react-native";
@@ -7,19 +9,31 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <View style={styles.container}>
-      <SearchInput />
-
+    <>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <SearchInput />
+        </View>
+        <SettingsIcon style={styles.icon} />
+      </View>
       <Button style={styles.button} onPress={() => router.replace("/")}>
         Back
       </Button>
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 50,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  inputContainer: {
+    flex: 1,
+  },
+  icon: {
+    marginRight: 24,
   },
   button: {
     marginTop: 50,
