81d164d66d94bc32e9c5277bf44b8565aa0163cb
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-12-12 13:06:31 +0100

    Change data source from mockup to API youtube

--- services/youtubeApi.ts
+++ services/youtubeApi.ts
@@ -1,5 +1,5 @@
-// const baseUrl = "https://www.googleapis.com/youtube/v3";
-const baseUrl = "http://192.168.55.103:3000/youtube/v3";
+const baseUrl = "https://www.googleapis.com/youtube/v3";
+// const baseUrl = "http://192.168.55.103:3000/youtube/v3";
 
 const youtubeAPIKey = process.env.EXPO_PUBLIC_YOUTUBE_API_KEY;
 
